02e1d70aae4053d7849f616d0926cbeb351e4a57
folador12 <luizantoniofoladorfilho@gmail.com>
2024-08-22 11:56:14 -0300

    first commit

---
+++ primeiroprojetospring/src/main/java/web/primeiroprojetospring/app/EsportesAppNormal.java
@@ -0,0 +1,7 @@
+package web.primeiroprojetospring.app;
+
+public class EsportesAppNormal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ primeiroprojetospring/src/main/java/web/primeiroprojetospring/app/EsportesAppSpring.java
@@ -0,0 +1,7 @@
+package web.primeiroprojetospring.app;
+
+public class EsportesAppSpring {
+    public static void main(String[] args) {
+
+    }
+}
